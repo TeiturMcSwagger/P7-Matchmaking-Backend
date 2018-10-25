5e2c800127682f32cda2a1e40ae5005ad77fb524
peter <pmadse13@student.aau.dk>
2018-10-25 19:34:14 +0200

    changed name of mongoUserService, and changed userController, so it is not coupled to mongoUserService but the interface.

--- lib/common/inversify.config.ts
+++ lib/common/inversify.config.ts
@@ -2,7 +2,7 @@ import { Container } from "inversify";
 import {BookService, GroupService, TYPES, UserService} from "../services/interfaces"
 import {ExampleService} from "../services/exampleService"
 import {MongoGroupService} from "../services/mongoGroupService"
-import { MongoUserService } from "../services/userService";
+import { MongoUserService } from "../services/mongoUserService";
 
 const AppContainer = new Container();
 AppContainer.bind<BookService>(TYPES.BookService).to(ExampleService)
--- lib/controllers/userController.ts
+++ lib/controllers/userController.ts
@@ -1,6 +1,5 @@
 import {Request, Response} from "express";
 
-import {MongoUserService} from "../services/userService";
 import { controller, httpGet, interfaces } from "inversify-express-utils";
 import { inject } from "inversify";
 import { TYPES, UserService } from "../services/interfaces";
@@ -17,13 +16,12 @@ export class UserController implements interfaces.Controller {
     }
 
     public async getUserById(req: Request, res: Response) : Promise<void>{
-        const userService: MongoUserService = new MongoUserService();
 
         let user_id : string = req.body.user_id;
 
         let result;
         try{
-            result = await userService.getUserById(user_id);
+            result = await this.userService.getUserById(user_id);
         }catch(error){
             result = error.message;
         }
@@ -32,14 +30,13 @@ export class UserController implements interfaces.Controller {
     }
 
     public async createUser(req: Request, res: Response): Promise<void>{
-        const userService: MongoUserService = new MongoUserService();
 
         let username = req.body.username;
 
         let result;
         try {
             // This is the created user response
-            result =  await userService.createUser(username);            
+            result =  await this.userService.createUser(username);            
         } catch (error) {
             // This is the error response
             result = error.message;
