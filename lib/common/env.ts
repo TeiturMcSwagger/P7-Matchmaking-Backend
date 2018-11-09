34dfd3369ca0a3cccedd6be4d2833bd4be96a158
clarse15 <clarse15@student.aau.dk>
2018-11-09 13:30:40 +0100

    Websocket (#24)
    * Improved backend architecture for sockets.IO and how events are registered
    
    * Minor fixes to WS architecture
    
    * Fixed bug that caused last event handler to be invoked on registration. Event handlers are now registered by their own name in their respective handler class. Having a function with a given name registeres this function as an event handler for an event with the same name.
    
    * Event handlers are more generically being constructed now. Also added acknowledgement callbacks (optional) in order to provide direct response to requests.
    
    * Updated packages
    
    * Changed response from joinGroup
    
    * WS visibility and merge conflictt fixed
    
    * Fixed minor error in groupController functionality
    
    * Added createGroup functionality
    
    * bla
    
    * Added leaveGroup functionality
    
    * Fixes after merge conflicts
    
    * small changes before masive merge hell

--- lib/common/env.ts
+++ lib/common/env.ts
@@ -1,8 +1,7 @@
 import * as dotenv from 'dotenv';
 
 const result = dotenv.config()
- 
-if (result.error) {
-    throw Error ("No .env file available. Create a .env file based on .env-template");
-}
 
+if (result.error) {
+    throw Error("No .env file available. Create a .env file based on .env-template");
+}
--- lib/services/interfaces.ts
+++ lib/services/interfaces.ts
@@ -1,31 +1,46 @@
-import { IGroup } from "models/groupModel";
-import { IUser } from "models/userModel";
+import { IGroup, IMongoGroup, IPersistedGroup } from "models/groupModel";
+import { IUser } from "models/userModel";
+import { Server } from 'socket.io';
+
 
 export interface BookService {
-  getAllBooks(): any;
+    getAllBooks(): any;
 }
+
 export interface GroupService {
-    updateGroupDiscordChannels(channels : string[], groupId : string);
     getGroups(): Promise<IGroup[]>;
-    updateGroupUsers(group_id: string, newUsers: string[]);
-    createGroup(group: any): Promise<IGroup>;
-    getGroup(group_id : String) : Promise<IGroup>;
-    getGroupsByUserId(user_id : String) : Promise<IGroup[]>;
     getFittingGroups(size: number, game: string): Promise<IGroup[]>;
-    leaveGroup(group_id : String, user_id : String) : any;
-    joinGroup(group_id : String, user_id : String) : any;
-    removeGroup(group_id: string);
+    createGroup(group: any): Promise<IMongoGroup>;
+    getGroup(group_id: String): Promise<IMongoGroup>;
+    leaveGroup(group_id: String, user_id: String): Promise<IMongoGroup>;
+    joinGroup(group_id: String, user_id: String): Promise<IMongoGroup>;
+    updateVisibility(group: IPersistedGroup): Promise<IMongoGroup>;
+    updateGroupDiscordChannels(channels: string[], groupId: string): Promise<IMongoGroup>;
+    updateGroupUsers(group_id: string, newUsers: string[]): Promise<IMongoGroup>;
+    getGroupsByUserId(user_id: String): Promise<IMongoGroup[]>;
+    removeGroup(group_id: string): Promise<IMongoGroup>;
 }
 
 export interface UserService {
-    getUserById(id : string) : Promise<IUser>
-    getAllUsers() : Promise<IUser[]>,
-    createUser(name: string, discordId : string) : IUser ,
-    getUserByDiscordId(discord_id : string) : any
+    getUserById(id: string): any
+    getAllUsers()
+    createUser(name: string, discordId: string): Promise<IUser>
+}
+
+export interface IIOService {
+    IO: Server;
+}
+
+export interface UserService {
+    getUserById(id: string): Promise<IUser>
+    getAllUsers(): Promise<IUser[]>,
+    createUser(name: string, discordId: string): IUser,
+    getUserByDiscordId(discord_id: string): any
 }
 
 export const TYPES = {
     BookService: Symbol.for("BookService"),
     UserService: Symbol.for("UserService"),
-    GroupService: Symbol.for("GroupService")
+    GroupService: Symbol.for("GroupService"),
+    IIOService: Symbol.for("IIOService"),
 };
